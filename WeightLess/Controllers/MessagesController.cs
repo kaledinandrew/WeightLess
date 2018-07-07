fe3eccf692ee1b4272dad2003bc5659dfdf39e25
raskolnikoff1147 <36156820+raskolnikoff1147@users.noreply.github.com>
2018-07-07 14:51:23 +0300

    version 1.5

---
+++ WeightLess/Controllers/MessagesController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using AdaptiveCards.Rendering;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using AdaptiveCards;
+
+namespace WeightLess
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else
+            {
+                HandleSystemMessageAsync(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private async Task<Activity> HandleSystemMessageAsync(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                IConversationUpdateActivity iConversationUpdated = message as IConversationUpdateActivity;
+                if (iConversationUpdated != null)
+                {
+                    ConnectorClient connector = new ConnectorClient(new System.Uri(message.ServiceUrl));
+
+                    foreach (var member in iConversationUpdated.MembersAdded ?? System.Array.Empty<ChannelAccount>())
+                    {
+                        // if the bot is added, then
+                        if (member.Id == iConversationUpdated.Recipient.Id)
+                        {
+                            HeroCard card = new HeroCard
+                            {
+                                Title = "Вас приветствует WeightLess бот.",
+                                Text = "Данный бот помогает следить за вашим здоровьем и балансом калорий в организме. Для начала работы с ботом отправьте любую\nпоследовательность символов",
+                            };
+                            Activity replyToConversation = message.CreateReply("");
+                            
+                            replyToConversation.Attachments.Add(card.ToAttachment());
+
+                            await connector.Conversations.SendToConversationAsync(replyToConversation);
+
+                        }
+                    }
+                }
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
